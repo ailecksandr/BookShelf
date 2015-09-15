c64dfd97b3a6d521f36a7598ac32f349c086f2ec
Alexandr Orlov <morozhunce@gmail.com>
2015-09-15 18:59:17 +0300

    Eeeeend

--- db/schema.rb
+++ db/schema.rb
@@ -41,8 +41,8 @@ ActiveRecord::Schema.define(version: 20150805115740) do
     t.text     "description", limit: 65535
     t.string   "image_url",   limit: 255
     t.decimal  "price",                     precision: 8, scale: 2
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
   end
 
   create_table "users", force: :cascade do |t|
